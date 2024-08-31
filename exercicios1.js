362c8ae65be94310b4ce172574f52533fa181de2
DaiRodri <daianarodri@gmail.com>
2024-08-30 21:29:42 -0300

    Add files via upload

---
+++ exercicios1.js
@@ -0,0 +1,8 @@
+let idade= 30;
+console.log("qual sua idade?"  + idade );
+
+let peso=30;
+console.log ("você tem quantos kg?"  + peso);
+
+const cidade= "Resende";
+console.log ("você mora a onde?"   + cidade)
